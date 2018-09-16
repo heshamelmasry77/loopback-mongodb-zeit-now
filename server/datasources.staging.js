90176ae888aa88e269a03accdadaca108fdf2cb3
hesham <heshamelmasry1992@gmail.com>
2018-09-16 23:52:29 +0200

    test

--- server/datasources.staging.js
+++ server/datasources.staging.js
@@ -14,18 +14,4 @@ module.exports = {
     url: process.env.DB_URL || "",
     "useNewUrlParser": "true"
   },
-  "initial": {
-    "cors": {
-      "params": {
-        "origin": function (origin, callback) {
-          console.log(origin);
-          if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-          } else {
-            callback(new Error('Not allowed by CORS'));
-          }
-        }
-      }
-    }
-  }
 };
