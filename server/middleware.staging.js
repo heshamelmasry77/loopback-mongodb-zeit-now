97bd1279f8a84d3270781625611cee0ae515ffdf
hesham <heshamelmasry1992@gmail.com>
2019-03-09 23:40:08 +0200

    no cors staging

--- server/middleware.staging.js
+++ server/middleware.staging.js
@@ -1,20 +1,20 @@
-const whitelist = ['https://room-me-front.herokuapp.com','http://room-me-front.herokuapp.com', 'https://room-me-front-staging.herokuapp.com','http://room-me-front-staging.herokuapp.com',
-'room-me-front-staging.herokuapp.com',
-'https://room-me-front-staging.herokuapp.com',
-    'localhost:3000'
-];
-module.exports = {
-  "initial": {
-    "cors": {
-      "params": {
-        "origin": function (origin, callback) {
-          if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-          } else {
-            callback(new Error('Not allowed by CORS staging'));
-          }
-        }
-      }
-    }
-  }
-};
+// const whitelist = ['https://room-me-front.herokuapp.com','http://room-me-front.herokuapp.com', 'https://room-me-front-staging.herokuapp.com','http://room-me-front-staging.herokuapp.com',
+// 'room-me-front-staging.herokuapp.com',
+// 'https://room-me-front-staging.herokuapp.com',
+//     'localhost:3000'
+// ];
+// module.exports = {
+//   "initial": {
+//     "cors": {
+//       "params": {
+//         "origin": function (origin, callback) {
+//           if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true);
+//           } else {
+//             callback(new Error('Not allowed by CORS staging'));
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
