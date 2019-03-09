6260b5ea339cf91b56d5a7d0140711aaf7cdbc99
hesham <heshamelmasry1992@gmail.com>
2019-03-10 00:22:08 +0200

    try allowed hosts

--- server/middleware.staging.js
+++ server/middleware.staging.js
@@ -1,14 +1,21 @@
-const whitelist = ['https://room-me-front.herokuapp.com','http://room-me-front.herokuapp.com','https://www.roome.co.za','http://www.roome.co.za','http://roome.co.za','https://roome.co.za','http://room-me-front-staging.herokuapp.com','https://room-me-front-staging.herokuapp.com'];
+const whitelist = ['https://room-me-front.herokuapp.com','http://room-me-front.herokuapp.com','https://www.roome.co.za','http://www.roome.co.za','http://roome.co.za','https://roome.co.za','http://room-me-front-staging.herokuapp.com','https://room-me-front-staging.herokuapp.com','http://localhost:3000'];
+
+const ALLOWED_HOSTS = ['https://room-me-front.herokuapp.com','http://room-me-front.herokuapp.com','https://www.roome.co.za','http://www.roome.co.za','http://roome.co.za','https://roome.co.za','http://room-me-front-staging.herokuapp.com','https://room-me-front-staging.herokuapp.com','http://localhost:3000'];
 module.exports = {
   "initial": {
     "cors": {
       "params": {
-        "origin": function (origin, callback) {
-          console.log(origin);
+        "origin": function(req, origin, callback) {
+
+          // note: if same origin makes requests with origin header, it needs to be whitelisted
+
           if (whitelist.indexOf(origin) !== -1) {
             callback(null, true);
+          } else if (origin === undefined && ALLOWED_HOSTS.indexOf(req.header.host)) {
+            callback(null, true);
           } else {
-            callback(new Error('Not allowed by CORS production'));
+            // callback(null, true)
+            callback(new Error(`${origin} not allowed by CORS`));
           }
         }
       }
