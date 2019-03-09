555360f87f636cbdb8f6d768d9b169dddc8b038e
hesham <heshamelmasry1992@gmail.com>
2019-03-09 23:58:55 +0200

    console origin

--- server/middleware.staging.js
+++ server/middleware.staging.js
@@ -6,6 +6,7 @@ module.exports = {
     "cors": {
       "params": {
         "origin": function (origin, callback) {
+          console.log(origin);
           if (whitelist.indexOf(origin) !== -1  || !origin) {
             callback(null, true);
           } else {
