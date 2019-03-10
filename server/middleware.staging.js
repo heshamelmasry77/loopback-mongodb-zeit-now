a79b375186a6aed7909388ec5addf2c0f64fdf03
hesham <heshamelmasry1992@gmail.com>
2019-03-10 11:06:28 +0200

    try

--- server/middleware.staging.js
+++ server/middleware.staging.js
@@ -1,17 +1,22 @@
-// const whitelist = ['http://localhost:3000'];
-// module.exports = {
-//   "initial": {
-//     "cors": {
-//       "params": {
-//         "origin": function (origin, callback) {
-//           console.log(origin);
-//           if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true);
-//           } else {
-//             callback(new Error('Not allowed by CORS production'));
-//           }
-//         }
-//       }
-//     }
-//   }
-// };
+const whitelist = ['http://localhost:3000'];
+module.exports = {
+  "initial": {
+    "cors": {
+      "params": {
+        "origin": function (req,origin, callback) {
+          console.log(req.headers.origin);
+          console.log(req.headers.host);
+          console.log(origin);
+          if(origin === undefined){
+            callback(null, true);
+          }
+          else if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+          } else {
+            callback(new Error('Not allowed by CORS production'));
+          }
+        }
+      }
+    }
+  }
+};
