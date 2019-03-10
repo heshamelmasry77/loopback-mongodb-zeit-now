39d55d5977aafe71c1d88499bdc8cda60e310873
hesham <heshamelmasry1992@gmail.com>
2019-03-10 15:30:06 +0200

    test

--- server/middleware.production.js
+++ server/middleware.production.js
@@ -5,7 +5,10 @@ module.exports = {
       "params": {
         "origin": function (origin, callback) {
           console.log(origin);
-          if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+          if (origin === undefined) {
+            callback(null, true);
+          }
+          else if (whitelist.indexOf(origin) !== -1) {
             callback(null, true);
           } else {
             callback(new Error('Not allowed by CORS production'));
