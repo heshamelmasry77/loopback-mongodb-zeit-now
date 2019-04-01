432503b74ce7ae00d760e1be07c858e5f1029a99
hesham <heshamelmasry1992@gmail.com>
2019-04-01 23:51:51 +0200

    using origin

--- server/middleware.production.js
+++ server/middleware.production.js
@@ -9,6 +9,8 @@ module.exports = {
       "params": {
         "origin": function (origin, callback) {
           console.log(origin);
+          origin = 'https://www.findaroome.com';
+          console.log('origin',origin);
            if (whitelist.indexOf(origin) !== -1) {
             callback(null, true);
           } else {
